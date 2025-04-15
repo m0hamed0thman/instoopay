d25f63bb586b9fe3e06b96cdb93b920a40e88f41
Mohamed Othman <othman9128@gmail.com>
2025-04-15 23:59:53 +0200

    Loading frame when the Program start

---
+++ src/main/java/Loading.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Loading extends JFrame {
+    public Loading() {
+        ImageIcon icon = new ImageIcon("src/main/resources/Transfergif.gif");
+        JLabel label = new JLabel(icon);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        this.getContentPane().add(label);
+        this.setUndecorated(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(1200, 630);
+        this.setLocationRelativeTo(null);
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,14 @@
 public class main {
     public static void main(String[] args) {
+        Loading loading = new Loading();
+        try {
+            loading.setVisible(true);
+            int time = 2000 ;
+            Thread.sleep(time);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        loading.setVisible(false);
         
     }
 }
