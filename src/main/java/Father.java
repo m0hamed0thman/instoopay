a64c7ba9a4ee39549edd1f6761d7ad4e3ed6b652
Mohamed Othman <othman9128@gmail.com>
2025-04-16 07:23:39 +0200

    The Father Class that All Frame Will inhertan from he /Don't extend from Jframe / use extend Father

---
+++ src/main/java/Father.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Father extends JFrame {
+    public Father() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(screenWidth/3 + screenWidth/6, screenHeight-50);
+        this.setLocationRelativeTo(null);
+    }
+}
