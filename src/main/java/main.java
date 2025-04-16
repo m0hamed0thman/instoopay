9f22b09ef95ed417c282327d6c78d67fe1e3ef3e
Mohamed Othman <othman9128@gmail.com>
2025-04-16 08:30:03 +0200

    Just try code in main not hard code

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,35 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class main {
-    public static void main(String[] args) {
-        Loading loading = new Loading();
+    private static boolean netIsAvailable() {
         try {
-            loading.setVisible(true);
-            int time = 2000 ;
-            Thread.sleep(time);
-        }catch (InterruptedException e) {
-            e.printStackTrace();
+            final URL url = new URL("http://www.google.com");
+            final URLConnection conn = url.openConnection();
+            conn.connect();
+            conn.getInputStream().close();
+            return true;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            return false;
         }
-        loading.setVisible(false);
-        
+    }
+
+    public static void main(String[] args) {
+//        Loading loading = new Loading();
+//        try {
+//            loading.setVisible(true);
+//            int time = 2000 ;
+//            Thread.sleep(time);
+//        }catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        loading.setVisible(false);
+        DBConnection db = new DBConnection();
+        db.getConnection();
+//        InternetMonitor.startMonitoring(1);
     }
 }
