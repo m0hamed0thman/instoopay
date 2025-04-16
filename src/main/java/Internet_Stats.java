23e13c2675677ca648e74ba656cda27816fc6adb
Mohamed Othman <othman9128@gmail.com>
2025-04-16 07:28:24 +0200

    internet status Frame working with Monitor class

---
+++ src/main/java/Internet_Stats.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Internet_Stats extends JFrame {
+    public Internet_Stats() {
+        ImageIcon icon = new ImageIcon("src/main/resources/no_internet.gif");
+
+        JLabel label = new JLabel(icon);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        this.getContentPane().add(label);
+        this.setUndecorated(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(500, 630);
+        this.setLocationRelativeTo(null);
+    }
+}
