274b682d4492cc64c72be601ee6e9ae99aca58cc
Mohamed Othman <othman9128@gmail.com>
2025-04-20 14:35:25 +0200

    generate a random code

---
+++ src/main/java/VerificationCodeGenerator.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class VerificationCodeGenerator {
+    public static String generateCode(int length) {
+        String digits = "0123456789";
+        StringBuilder code = new StringBuilder();
+        Random rnd = new Random();
+        for (int i = 0; i < length; i++) {
+            code.append(digits.charAt(rnd.nextInt(digits.length())));
+        }
+        return code.toString();
+    }
+}
